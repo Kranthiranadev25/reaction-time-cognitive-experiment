5bacf5712c03d7d25b6564a7d8b570b2e6a5ecd7
Kranthiranadev25 <kranthimittapelly@gmail.com>
2026-03-08 00:24:14 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -5,5 +5,11 @@ data = pd.read_csv("reaction_data.csv")
 normal_mean = data["normal"].mean()
 distraction_mean = data["distraction"].mean()
 
+normal_std = data["normal"].std()
+distraction_std = data["distraction"].std()
+
 print("Average Reaction Time (Normal):", normal_mean)
 print("Average Reaction Time (Distraction):", distraction_mean)
+
+print("Standard Deviation (Normal):", normal_std)
+print("Standard Deviation (Distraction):", distraction_std)
